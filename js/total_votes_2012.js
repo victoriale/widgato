3fed4185c41d366d97c7f3508f857327d1a2dcd7
victoriale <le.victoriavan@gmail.com>
2015-11-08 22:15:51 -0600

    added remnant code for myhousekit

--- js/total_votes_2012.js
+++ js/total_votes_2012.js
@@ -2,7 +2,57 @@ var offset = 0;
 var dataLength;
 var curData;
 
+var domain = '';
+var clickyId = 0;
+var remnant = '';
+var locName = '';
+var city = '';
+var state = '';
+var loc = '';
+var max = 10;
+var bord = false;
 $(function(){
+  var temp = location.search;
+  var query = {};
+
+  if(temp != null){
+    query = JSON.parse(decodeURIComponent(temp.substr(1)));
+
+    //set the query data from database to global variable to use
+    domain = query.dom;
+
+    remnant = query.remn;
+
+    clickyId = query.c_id;
+
+    locName = query['loc']['loc_name'];
+
+    locName = locName.replace('+',' ');
+    //returns string true or false
+    bord = query.bord;
+		/*
+		//Same as domain = query.dom  but if that doesnt work this should work so USE [loc] global variable
+		//USE BOTTOM ONCE WE IMPLEMENT MULTIPLE CITIES INTO LIST PAGE
+		for(var i = 0; i < query['loc']['loc']['city'].length; i++){
+			var c = query['loc']['loc']['city'][i].city;
+			var s = query['loc']['loc']['city'][i].state;
+			loc = loc + c + "," + s;
+			if (typeof query['loc']['loc']['city'][i+1] != 'undefined'){
+				loc += '|';
+			}
+		}
+		*/
+  	}
+
+  	if(bord == 'true'){
+  		$(".re_w_list").css({'border-right':'1px solid #ccc','border-bottom':'1px solid #ccc','border-left':'1px solid #ccc'});
+  	}
+
+  	var script_tag = document.createElement('script');
+  	script_tag.setAttribute('src','//static.getclicky.com/js');
+  	document.head.appendChild(script_tag);
+  	var clicks = $('<script>try{ clicky.init('+clickyId+'); }catch(e){}</script>');
+  	document.head.appendChild(clicks[0]);
 
     $('.tv-rightnav').on('click', function() {
         if (offset < dataLength-1 && $(this).data('dir') === 'next') {
@@ -22,21 +72,26 @@ $(function(){
     });
 
   	$.get('http://api.synapsys.us/rt/index.php?widget=politics&wid=1&city=Wichita&state=KS', function(data){
-      console.log(data);
       curData = data.widget;
       dataLength = curData.length;
       dataCall(offset);
     }, 'json')
   })//END OF FUNCTION
   function dataCall(index){
-    var link ="http://localhost:3000/";
+    var link ="http://www.myhousekit.com/";
     var title = "counties-with-the-most-number-of-total-votes-in-the-2012-election";
     $('.tv-t2-loc').html(curData[index].county+' County, '+curData[index].state);
     $('.tv-content1').html(dNumberToCommaNumber(curData[index].votes)+' Votes');
 		$('.tv-image').css('background', 'url('+curData[index].image+') no-repeat');
-    $('#county').attr('href',link+curData[index].state+"/"+curData[index].county+"/county");
-		$('.tv-href').attr('href',link+title+"/"+curData[index].state+"/"+curData[index].county+"/politics");
-		$('#loc').attr('href',link+curData[index].state+"/"+curData[index].county+"/county");
+    if(remnant == 'true' || remnant == true){
+      $('.tv-href').attr('href',link+title+"/"+curData[index].state+"/"+curData[index].county+"/politics");
+      $('#loc').attr('href',link+curData[index].state+"/"+curData[index].county+"/county");
+      $('#county').attr('href',link+curData[index].state+"/"+curData[index].county+"/county");
+    } else {
+      $('.tv-href').attr('href',link+domain+"/"+title+"/"+curData[index].state+"/"+curData[index].county+"/politics");
+      $('#loc').attr('href',link+domain+"/"+curData[index].state+"/"+curData[index].county+"/county");
+      $('#county').attr('href',link+domain+"/"+curData[index].state+"/"+curData[index].county+"/county");
+    }
 
     //$('#loc').attr('href',link+"location/"+toUpperCase(curData[index].county)+"_"+toUpperCase(curData[index].state));
   }
