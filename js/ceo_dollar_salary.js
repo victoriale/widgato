f0d9ac105bc97e615d51929bf9b39363303eefa7
LarryHPham <larrypham1991@yahoo.com>
2015-11-17 12:57:16 -0600

    moved old file and ceo_dollar remove console log

--- js/ceo_dollar_salary.js
+++ js/ceo_dollar_salary.js
@@ -73,7 +73,6 @@ $(function(){
 
 	$.get('http://apifin.investkit.com/call_controller.php?action=widget&option=ceo_make_one_dollar', function(data){
     data_result = data.ceo_make_one_dollar;
-    console.log(data);
     curData = data_result.list_data;
     dataLength = curData.length;
     dataCall(offset);
@@ -86,7 +85,6 @@ function dataCall(index){
   $('.cds-loc').html(curData[index].c_hq_city+' '+curData[index].c_hq_state);
   $('#paid').html(nFormatter(curData[index].TotalComp));
   $('.cds-image').css('background', 'url('+imageUrl(curData[index].o_pic)+') no-repeat');
-  console.log(curData);
   $('.cds-share').css('background', 'url('+imageUrl(curData[index].c_logo)+') no-repeat');
   //$('.cds-image').css('background','url(http://images.investkit.com/images/'+curData[index].o_pic+') no-repeat');
   if(remnant == 'true' || remnant == true){
