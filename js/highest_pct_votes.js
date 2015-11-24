924a3b20e90d244782eba6fb5e9122627dffce47
LarryHPham <larrypham1991@yahoo.com>
2015-11-23 18:58:04 -0600

    political widget got rid of http and https to let broswer decide. reduce overhead

--- js/highest_pct_votes.js
+++ js/highest_pct_votes.js
@@ -73,7 +73,7 @@ $(function(){
 
     if(city == null || typeof city == 'undefined' || state == null || typeof state == 'undefined'){
       if(remnant == 'true' || remnant === true){
-        $.get("https://apireal.synapsys.us/listhuv/?action=get_remote_addr2",function(r_data){
+        $.get("//apireal.synapsys.us/listhuv/?action=get_remote_addr2",function(r_data){
           city = r_data[0].city;
           state = r_data[0].state;
 
@@ -86,7 +86,7 @@ $(function(){
         });
       }else{
         //partner with no data same thing as if statement but doing this just in case
-        $.get("https://apireal.synapsys.us/listhuv/?action=get_remote_addr2",function(r_data){
+        $.get("//apireal.synapsys.us/listhuv/?action=get_remote_addr2",function(r_data){
           city = r_data[0].city;
           state = r_data[0].state;
 
