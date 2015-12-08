603cebe12c27edff8a0cc950817adbb5267fc26b
victoriale <le.victoriavan@gmail.com>
2015-12-08 14:48:12 -0600

    comma style change

--- js/robberies.js
+++ js/robberies.js
@@ -74,7 +74,7 @@ $(function(){
       dataLength = curData.length;
       var title = "most-robberies-by-city";
       $('.fcw-t1').html('Cities in' + fullstate(curData[0].CrimeState) + ' with the Most Property Crimes');
-      $('.fcw-t2-loc').html(curData[0].CrimeCity+' ,'+curData[0].CrimeState);
+      $('.fcw-t2-loc').html(curData[0].CrimeCity+', '+curData[0].CrimeState);
       $('.fcw-img2').html('#'+(index+1));
       $('.fcw-content1').html((curData[0].CrimeLarcenyNumber).replace(/\B(?=(\d{3})+(?!\d))/g, ",") +' Robberies');
       $('.fcw-content2').html('in ' + curData[0].CrimeYear);
