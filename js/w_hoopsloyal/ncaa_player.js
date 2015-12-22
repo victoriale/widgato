2a3b7567bfe13bb6c00e8911089854a44377e742
victoriale <le.victoriavan@gmail.com>
2015-12-22 16:25:35 -0600

    group files, check for partner with no location, delete commented codeds that no long need

---
+++ js/w_hoopsloyal/ncaa_player.js
@@ -0,0 +1,89 @@
+var offset = 0;
+var dataLength;
+var curData;
+var domain = '';
+var clickyId = 0;
+var remnant = '';
+var max = 10;
+var bord = false;
+var link = "http://pnsports.synapsys.us";
+//var link_partner = "http://localhost:3000/";
+$(function(){
+  var temp = location.search;
+  var query = {};
+  if(temp != null){
+    query = JSON.parse(decodeURIComponent(temp.substr(1)));
+    domain = query.dom;
+    remnant = query.remn;
+    bord = query.bord;
+  	}
+
+  	if(bord == 'true'){
+  		$(".re_w_list").css({'border-right':'1px solid #ccc','border-bottom':'1px solid #ccc','border-left':'1px solid #ccc'});
+  	}
+    $('.fcw-rightnav').on('click', function() {
+        if (offset < dataLength-1 && $(this).data('dir') === 'next') {
+            dataCall(++offset);
+        }
+    });
+
+    $('.fcw-leftnav').on('click', function() {
+        if (offset > 0 && $(this).data('dir') === 'prev') {
+            dataCall(--offset);
+        }else if(offset <= 0){
+          offset = 0;
+          dataCall(offset);
+        }
+    });
+
+    $.get('//apisports.synapsys.us:91/NBAHoops/call_controller.php?scope=ncaa&action=widgets&option=player_widget', function(data){
+      curData = data;
+      dataCall(offset);
+    }, 'json');
+  })//END OF FUNCTION
+
+  function dataCall(index){
+      var wData = curData.player_widget;
+      var listData = wData.list_data;
+      dataLength = listData.length;
+      var title = (wData.list_title).replace('/','-').replace(/\s+/g, '-');
+      var team_image = listData[index].team_firstname + ' ' + listData[index].team_lastname;
+      team_image = team_image.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'').replace('amp;', '').replace('-',' ');
+      var teamName = team_image.replace(/\s+/g, '-');
+      team_image = team_image.replace(/ /g, '_');
+      var player_url = listData[index].player_first_name+'-'+listData[index].player_last_name;
+      $('.fcw-t1').html(wData.list_title);
+      $('.fcw-t2-title').html(listData[index].player_last_name + ', ' + listData[index].player_first_name);
+      if(listData[index].player_position == null || listData[index].player_position == ''){
+        $('.fcw-loc').html('N/A');
+      } else {
+        $('.fcw-loc').html(listData[index].player_position);
+      }
+      $('.fcw-image').css('background', 'url('+imageUrl(listData[index].player_img)+') no-repeat');
+      $('.fcw-logo').css('background', 'url('+teamImage(team_image)+') no-repeat');      $('.fcw-t2-num').html('#'+(index+1));
+      // var w_value = listData[index][wData.list_metric];
+      $('.fcw-content1').html(listData[index].formatted_metric + ' ' + listData[index].friendly_metric);
+      $('.fcw-content2').html('in the 2015 Season');
+
+      if(remnant == 'true' || remnant == true){
+        $('#title_link').attr('href',link+ "/NCAA/player/" + teamName + "/"+ player_url+ "/" + listData[index].PlayerId);
+        $('.exec-link').attr('href',link+ "/NCAA/player/"+teamName+"/" + player_url + "/" + listData[index].PlayerId);
+        $('#teamProfile').attr('href',link + "/NCAA/team/" + teamName + "/" + listData[index].team_id);
+        $('.fcw-href').attr('href',link + "NCAA/player/" + title + "/" + wData.list_id+"/listview/1");
+       } else {
+        $('#title_link').attr('href',link+ "/" + domain + "/NCAA/p/"+teamName+"/"+listData[index].player_last_name+'-'+listData[index].player_first_name+"/"+listData[index].PlayerId);
+        $('.exec-link').attr('href',link+ "/" + domain + "/NCAA/p/"+teamName+"/"+listData[index].player_last_name+'-'+listData[index].player_first_name+"/"+listData[index].PlayerId);
+        $('#teamProfile').attr('href',link + "/NCAA/team/" + teamName + "/" + listData[index].team_id);
+        $('.fcw-href').attr('href',link + "/" + domain + "/NCAA/player/" + title + "/list/" + wData.list_id+"/1");
+      }
+  }
+
+function imageUrl(path){
+  if(typeof path == 'undefined' || path == null || path == '' || path == 'null'){
+    return '../css/public/no_image.jpg';
+  }
+  return 'http://sports-images.synapsys.us:99' + path;
+}
+function teamImage(tpath){
+  return 'http://sports-images.synapsys.us:99/ncaab/logos/NCAAB_'+ tpath + '_Logo.jpg';
+}
---
+++ js/w_hoopsloyal/ncaa_team.js
@@ -0,0 +1,75 @@
+var offset = 0;
+var dataLength;
+var curData;
+var domain = '';
+var remnant = '';
+var max = 10;
+var bord = false;
+var link = "http://pnsports.synapsys.us";
+$(function(){
+  var temp = location.search;
+  var query = {};
+  if(temp != null){
+    query = JSON.parse(decodeURIComponent(temp.substr(1)));
+    domain = query.dom;
+    remnant = query.remn;
+    bord = query.bord;
+  	}
+
+
+  	if(bord == 'true'){
+  		$(".re_w_list").css({'border-right':'1px solid #ccc','border-bottom':'1px solid #ccc','border-left':'1px solid #ccc'});
+  	}
+    $('.fcw-rightnav').on('click', function() {
+        if (offset < dataLength-1 && $(this).data('dir') === 'next') {
+            dataCall(++offset);
+        }
+    });
+
+    $('.fcw-leftnav').on('click', function() {
+        if (offset > 0 && $(this).data('dir') === 'prev') {
+            dataCall(--offset);
+        }else if(offset <= 0){
+          offset = 0;
+          dataCall(offset);
+        }
+    });
+
+    $.get('//apisports.synapsys.us:91/NBAHoops/call_controller.php?scope=ncaa&action=widgets&option=team_widget', function(data){
+      curData = data;
+      dataCall(offset);
+    }, 'json');
+  })//END OF FUNCTION
+
+  function dataCall(index){
+      var wData = curData.team_widget;
+      var listData = wData.list_data;
+      dataLength = listData.length;
+      var title = (wData.list_title).replace('/','-').replace(/\s+/g, '-');
+      var team_image = listData[index].team_name;
+      team_image = team_image.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'').replace('amp;', '').replace('-',' ');
+      team_image = team_image.replace(/ /g, '_');
+      var w_value = listData[index][wData.list_metric];
+      var teamName = (listData[index].team_name).replace(/\s+/g, '-');
+      $('.fcw-t1').html(wData.list_title);
+      $('.fcw-t2-title').html(listData[index].team_name);
+      $('.fcw-loc').html(listData[index].player_position);
+      $('.fcw-image').css('background', 'url('+teamImage(team_image)+') no-repeat');
+      $('.fcw-t2-num').html('#'+(index+1));
+      $('.fcw-content1').html(listData[index].formatted_metric + ' ' + listData[index].friendly_metric);
+      $('.fcw-content2').html('in the 2015 Season');
+
+      if(remnant == 'true' || remnant == true){
+        $('#title_link').attr('href',link + "/NCAA/team/" + teamName + "/" + listData[index].TeamID);
+        $('.exec-link').attr('href',link + "/NCAA/team/" + teamName + "/" + listData[index].TeamID);
+        $('.fcw-href').attr('href',link + "/NCAA/team/" + title + "/" + wData.list_id + "/listview/1");
+      } else {
+        $('#title_link').attr('href',link + "/" + domain + "/NCAA/t/" + teamName + "/" + listData[index].TeamID);
+        $('.exec-link').attr('href',link + "/" + domain + "/NCAA/t/" + teamName + "/" + listData[index].TeamID);
+        $('.fcw-href').attr('href',link + "/" + domain + "/NCAA/team/" + title + "/list/" + wData.list_id + "/1");
+      }
+  }
+
+function teamImage(tpath){
+  return 'http://sports-images.synapsys.us:99/ncaab/logos/NCAAB_'+ tpath + '_Logo.jpg';
+}
