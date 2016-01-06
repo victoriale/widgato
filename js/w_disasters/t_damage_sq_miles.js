5888e22bc279f797dc76ba89bf4d6953c1010999
victoriale <le.victoriavan@gmail.com>
2016-01-06 10:52:02 -0600

    tornado widgets are done, css for earthquake widget, waiting on api

---
+++ js/w_disasters/t_damage_sq_miles.js
@@ -0,0 +1,76 @@
+var offset = 0;
+var dataLength;
+var curData;
+var domain = '';
+var remnant = '';
+var max = 10;
+var bord = false;
+var apiUrl = '//devapirt.synapsys.us';
+var wid_num = 9;
+var link = "http://www.joyfulhome.com/";
+var link_partner = "http://www.myhousekit.com/";
+$(function(){
+  var temp = location.search;
+  var query = {};
+  if(temp != null){
+    query = JSON.parse(decodeURIComponent(temp.substr(1)));
+    domain = query.dom;
+    remnant = query.remn;
+    bord = query.bord;
+  	}
+
+  	if(bord == 'true'){
+  		$(".re_w_list").css({'border-right':'1px solid #ccc','border-bottom':'1px solid #ccc','border-left':'1px solid #ccc'});
+  	}
+
+    $('.fcw-rightnav').on('click', function() {
+        if ($(this).data('dir') === 'next') {
+            dataCall(++offset);
+        }
+    });
+
+    $('.fcw-leftnav').on('click', function() {
+        if (offset > 0 && $(this).data('dir') === 'prev') {
+              dataCall(--offset);
+        }else if(offset <= 0){
+          offset = 0;
+          dataCall(offset);
+        }
+    });
+    dataCall(offset);
+  })//END OF FUNCTION
+  function dataCall(index){
+  	$.get(apiUrl + '/index.php?widget=disasters&wid='+wid_num+'&skip='+index+'&limit=1', function(data){
+      var curData = data.widget;
+      dataLength = curData.length;
+      var title = "tornado-damage-by-sq-miles";
+      $('#title').html('Tornado with Biggest Damage Sq Mileage');
+      $('.fcw-t1').html('Toradoes with the Biggest Damage Square Mileage ');
+      $('.fcw-t2-loc').html(curData[0].City+', '+curData[0].State);
+      $('.fcw-image').css('background', 'url('+imageUrl(curData[0].img)+') no-repeat');
+      $('.fcw-img2').html('#'+(index+1));
+      if(curData[0].SqMilesDamage == 1){
+        $('.fcw-content1').html(Number(curData[0].SqMilesDamage).toFixed(2) + ' Mile');
+      } else {
+        $('.fcw-content1').html(Number(curData[0].SqMilesDamage).toFixed(2) + ' Miles');
+      }
+      $('.fcw-content2').html(curData[0].Month + ' ' + curData[0].Day + ', ' + curData[0].Year);
+      $('.fcw-presentedby').html('BIGGEST DAMAGE SQ MILES TORNADO- PRESENTED BY');
+      if(remnant == 'true' || remnant == true){
+        $('.fcw-href').attr('href',link+title+"/"+curData[0].State+"/national/disasters");
+        $('#loc').attr('href',link+"location/"+(curData[0].City).toUpperCase()+"_"+curData[0].State);
+        $('#imgUrl').attr('href',link+"location/"+(curData[0].City).toUpperCase()+"_"+curData[0].State);
+      } else {
+        $('.fcw-href').attr('href',link_partner+domain+"/national/disasters/"+title+"/"+curData[0].State);
+        $('#loc').attr('href',link_partner+domain+"/loc/"+curData[0].State+"/"+(curData[0].City).toUpperCase());
+        $('#imgUrl').attr('href',link_partner+domain+"/loc/"+curData[0].State+"/"+(curData[0].City).toUpperCase());
+      }
+    }, 'json')
+  }
+
+  function imageUrl(path){
+    if(typeof path == 'undefined' || path == null || path == '' || path == 'null' || path == "http://apireal.synapsys.us/city-image/images/placeholder-location.jpg"){
+      return '../css/public/no_image.jpg';
+    }
+    return path;
+  }
---
+++ js/w_disasters/t_highest_risk.js
@@ -0,0 +1,76 @@
+var offset = 0;
+var dataLength;
+var curData;
+var domain = '';
+var remnant = '';
+var max = 10;
+var bord = false;
+var apiUrl = '//devapirt.synapsys.us';
+var wid_num = 6;
+var link = "http://www.joyfulhome.com/";
+var link_partner = "http://www.myhousekit.com/";
+$(function(){
+  var temp = location.search;
+  var query = {};
+  if(temp != null){
+    query = JSON.parse(decodeURIComponent(temp.substr(1)));
+    domain = query.dom;
+    remnant = query.remn;
+    bord = query.bord;
+  	}
+
+  	if(bord == 'true'){
+  		$(".re_w_list").css({'border-right':'1px solid #ccc','border-bottom':'1px solid #ccc','border-left':'1px solid #ccc'});
+  	}
+
+    $('.fcw-rightnav').on('click', function() {
+        if ($(this).data('dir') === 'next') {
+            dataCall(++offset);
+        }
+    });
+
+    $('.fcw-leftnav').on('click', function() {
+        if (offset > 0 && $(this).data('dir') === 'prev') {
+              dataCall(--offset);
+        }else if(offset <= 0){
+          offset = 0;
+          dataCall(offset);
+        }
+    });
+    dataCall(offset);
+  })//END OF FUNCTION
+  function dataCall(index){
+  	$.get(apiUrl + '/index.php?widget=disasters&wid='+wid_num+'&skip='+index+'&limit=1', function(data){
+      var curData = data.widget;
+      dataLength = curData.length;
+      var title = "tornado-highest-risk-level";
+      $('#title').html('Highest Risk of Having Tornado');
+      $('.fcw-t1').html('Cities with the Highest Risk of Experiencing a Tornado');
+      $('.fcw-t2-loc').html(curData[0].City+', '+curData[0].State);
+      $('.fcw-image').css('background', 'url('+imageUrl(curData[0].img)+') no-repeat');
+      $('.fcw-img2').html('#'+(index+1));
+      if(curData[0].NumOfTornadoes == 1){
+        $('.fcw-content1').html(curData[0].NumOfTornadoes + ' Tornado');
+      } else {
+        $('.fcw-content1').html(curData[0].NumOfTornadoes + ' Tornadoes');
+      }
+      $('.fcw-content2').html('Per Year');
+      $('.fcw-presentedby').html('CITIES WITH HIGHEST TORNADO RISK - PRESENTED BY');
+      if(remnant == 'true' || remnant == true){
+        $('.fcw-href').attr('href',link+title+"/"+curData[0].State+"/national/disasters");
+        $('#loc').attr('href',link+"location/"+(curData[0].City).toUpperCase()+"_"+curData[0].State);
+        $('#imgUrl').attr('href',link+"location/"+(curData[0].City).toUpperCase()+"_"+curData[0].State);
+      } else {
+        $('.fcw-href').attr('href',link_partner+domain+"/national/disasters/"+title+"/"+curData[0].State);
+        $('#loc').attr('href',link_partner+domain+"/loc/"+curData[0].State+"/"+(curData[0].City).toUpperCase());
+        $('#imgUrl').attr('href',link_partner+domain+"/loc/"+curData[0].State+"/"+(curData[0].City).toUpperCase());
+      }
+    }, 'json')
+  }
+
+  function imageUrl(path){
+    if(typeof path == 'undefined' || path == null || path == '' || path == 'null' || path == "http://apireal.synapsys.us/city-image/images/placeholder-location.jpg"){
+      return '../css/public/no_image.jpg';
+    }
+    return path;
+  }
