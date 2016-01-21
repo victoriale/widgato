4f6d121736aace6c784d67bc9e8dea8270e280b0
victoriale <le.victoriavan@gmail.com>
2016-01-21 17:06:15 -0600

    remove http

--- js/w_hoopsloyal/nba_player_new.js
+++ js/w_hoopsloyal/nba_player_new.js
@@ -36,7 +36,7 @@ $(function(){
           dataCall(offset);
         }
     });
-    $.get('http://prod-sports-api.synapsys.us/NBAHoops/call_controller.php?scope=nba&action=widgets&option=player_widget', function(data){
+    $.get('//prod-sports-api.synapsys.us/NBAHoops/call_controller.php?scope=nba&action=widgets&option=player_widget', function(data){
       curData = data;
       dataCall(offset);
     }, 'json');
