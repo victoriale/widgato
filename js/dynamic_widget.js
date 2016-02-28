54993a667f3f4c725a2a12bf04530f37adc5485c
William <willyk95@gmail.com>
2016-02-28 10:52:25 -0600

    Change how 'random' the lists are - make earlier numbers more likely to appear

--- js/dynamic_widget.js
+++ js/dynamic_widget.js
@@ -12,9 +12,20 @@ dynamic_widget = (function(){
       widget_conf.category = "finance";
     }
 
+    // Generate a weighted random number
+    var random = [
+      0, 0, 0,
+      1, 1, 1,
+      2, 2, 2,
+      3, 3,
+      4, 4,
+      5, 5,
+      6, 7, 8, 9]; // Weighted array - leaning towards the first lists
+    var random = random[Math.floor(Math.random() * random.length)];
+
     // Call the API
     $.ajax({
-      url: api_url + '?partner=' + (typeof(widget_conf.dom) != "undefined" ? widget_conf.dom : "") + '&cat=' + widget_conf.category + '&rand=' + Math.floor(Math.random() * 10),
+      url: api_url + '?partner=' + (typeof(widget_conf.dom) != "undefined" ? widget_conf.dom : "") + '&cat=' + widget_conf.category + '&rand=' + random,
       dataType: 'json',
       success: function(data) {
         console.log(data);
