1fb10615a5328546965641c1f9a468af2cbb8504
lynchtroop5 <christopher.lynch@sntmedia.com>
2016-04-05 08:41:32 -0500

    Temporary fix to get the widget working.

--- js/ai_article.js
+++ js/ai_article.js
@@ -11,7 +11,7 @@ ai_widget = (function() {
   } else {
     scope = 'nba';
   }
-  var APIUrl = 'http://dev-sports-ai.synapsys.us/' + scope + '/widget',
+  var APIUrl = 'http://dev-sports-ai.synapsys.us/' + scope + '/widget/63217',
     AIData = {},
     gameID = -1,
     pageInd = -1,
