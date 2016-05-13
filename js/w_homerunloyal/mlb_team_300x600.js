0cac38576977181b6d3914cc6f22e6cf6b0ab597
victoriale <le.victoriavan@gmail.com>
2016-05-13 13:57:10 -0500

    populate with mlb apis, still have questions for design about data point

--- js/w_homerunloyal/mlb_team_300x600.js
+++ js/w_homerunloyal/mlb_team_300x600.js
@@ -1,5 +1,3 @@
-// will change when api is available
-
 var offset = 0;
 var dataLength;
 var curData;
@@ -7,11 +5,15 @@ var domain = '';
 var remnant = '';
 var max = 10;
 var bord = false;
-var link = "http://www.hoopsloyal.com";
-var partner_link = "http://www.myhoopszone.com";
+var protocolToUse = (location.protocol == "https:") ? "https" : "http";
+var link = protocolToUse+"://www.homerunloyal.com/";
+var link_partner = protocolToUse+"://www.homerunzone.com/";
+var apiUrl = protocolToUse+'://dev-homerunloyal-api.synapsys.us/';
 $(function(){
+
   var temp = location.search;
   var query = {};
+
   if(temp != null){
     query = JSON.parse(decodeURIComponent(temp.substr(1)));
     domain = query.dom;
@@ -25,6 +27,9 @@ $(function(){
     $('.fcw-rightnav').on('click', function() {
         if (offset < dataLength-1 && $(this).data('dir') === 'next') {
             dataCall(++offset);
+        }else if(offset >= dataLength-1){
+          offset = 0;
+          dataCall(offset);
         }
     });
 
@@ -32,50 +37,38 @@ $(function(){
         if (offset > 0 && $(this).data('dir') === 'prev') {
             dataCall(--offset);
         }else if(offset <= 0){
-          offset = 0;
+          offset = dataLength-1;
           dataCall(offset);
         }
     });
-
-    $.get('http://prod-sports-api.synapsys.us/NBAHoops/call_controller.php?scope=nba&action=widgets&option=team_widget', function(data){
-      curData = data;
+    $.get(apiUrl+'randomList/team/20/1', function(data){
+      curData = data.data;
       dataCall(offset);
     }, 'json');
   })//END OF FUNCTION
 
   function dataCall(index){
-      var wData = curData.team_widget;
-      var listData = wData.list_data;
+      var listInfo = curData.listInfo;
+      var listData = curData['listData'];
       dataLength = listData.length;
-      var title = (wData.list_title).replace('/','-').replace(/\s+/g, '-');
-      var team_image = listData[index].team_name;
-      team_image = team_image.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'').replace('amp;', '').replace('-',' ');
-      team_image = team_image.replace(/ /g, '_');
-      var w_value = listData[index][wData.list_metric];
-      var teamName = (listData[index].team_name).replace(/\s+/g, '-');
-      $('.fcw-t1').html(wData.list_title);
-      $('.fcw-t2-title').html(listData[index].team_name);
-      $('.fcw-loc').html(listData[index].player_position);
-      $('.fcw-image').css('background', 'url('+teamImage(team_image)+') no-repeat');
+      $('.fcw-t1').html(listInfo.name);
       $('.fcw-t2-num').html('#'+(index+1));
-      $('.fcw-content1').html('[Team Name]');
-      $('#fcw-content2a').html('[Stadium Name]');
-      $('#fcw-content2b').html('[Location]');
-      $('.fcw-content3').html('[Data Point 3] [Data Value 3] for [YYYY]');
+      $('.fcw-image').css('background', 'url('+imageUrl(listData[index].teamLogo)+') no-repeat');
+      $('.fcw-content1').html(listData[index].teamName);
+      $('#fcw-content2a').html(listData[index].teamVenue);
+      $('#fcw-content2b').html(listData[index].teamState);
+      $('.fcw-content3').html('[Data Point 2] [Data Value 2] for [YYYY]');
 
       if(remnant == 'true' || remnant == true){
-        $('.fcw-icon').attr('href',link);
-        $('#title_link').attr('href',link + "/NBA/team/" + teamName + "/" + listData[index].TeamID);
-        $('.exec-link').attr('href',link + "/NBA/team/" + teamName + "/" + listData[index].TeamID);
-        $('.fcw-href').attr('href',link + "/NBA/team/" + title + "/" + wData.list_id + "/listview/1");
+        //TODO
       } else {
-        $('.fcw-icon').attr('href',partner_link + "/" + domain);
-        $('#title_link').attr('href',partner_link + "/" + domain + "/NBA/t/" + teamName + "/" + listData[index].TeamID);
-        $('.exec-link').attr('href',partner_link + "/" + domain + "/NBA/t/" + teamName + "/" + listData[index].TeamID);
-        $('.fcw-href').attr('href',partner_link + "/" + domain + "/NBA/team/" + title + "/list/" + wData.list_id + "/1");
+        //TODO
       }
   }
 
-function teamImage(tpath){
-  return 'http://prod-sports-images.synapsys.us/nba/logos/NBA_'+ tpath + '_Logo.jpg';
+function imageUrl(path){
+  if(typeof path == 'undefined' || path == null || path == '' || path == 'null'){
+    return 'http://prod-sports-images.synapsys.us/nba/players/headshots/no_player_icon.png';
+  }
+  return 'http://prod-sports-images.synapsys.us' + path;
 }
--- js/w_homerunloyal/mlb_team_970x250.js
+++ js/w_homerunloyal/mlb_team_970x250.js
@@ -1,4 +1,3 @@
-//will need to update when api is ready
 var offset = 0;
 var dataLength;
 var curData;
@@ -6,8 +5,10 @@ var domain = '';
 var remnant = '';
 var max = 10;
 var bord = false;
-var link = "http://www.homerunloyal.com";
-var partner_link = "http://www.homerunzone.com";
+var protocolToUse = (location.protocol == "https:") ? "https" : "http";
+var link = protocolToUse+"://www.homerunloyal.com/";
+var link_partner = protocolToUse+"://www.homerunzone.com/";
+var apiUrl = protocolToUse+'://dev-homerunloyal-api.synapsys.us/';
 $(function(){
 
   var temp = location.search;
@@ -26,6 +27,9 @@ $(function(){
     $('.fcw-rightnav').on('click', function() {
         if (offset < dataLength-1 && $(this).data('dir') === 'next') {
             dataCall(++offset);
+        }else if(offset >= dataLength-1){
+          offset = 0;
+          dataCall(offset);
         }
     });
 
@@ -33,55 +37,35 @@ $(function(){
         if (offset > 0 && $(this).data('dir') === 'prev') {
             dataCall(--offset);
         }else if(offset <= 0){
-          offset = 0;
+          offset = dataLength-1;
           dataCall(offset);
         }
     });
-    //will need to update when api is ready, using nba api
-    $.get('http://prod-sports-api.synapsys.us/NBAHoops/call_controller.php?scope=nba&action=widgets&option=player_widget', function(data){
-      curData = data;
+
+    $.get(apiUrl+'randomList/team/20/1', function(data){
+      curData = data.data;
       dataCall(offset);
     }, 'json');
   })//END OF FUNCTION
 
   function dataCall(index){
-      var wData = curData.player_widget;
-      var listData = wData.list_data;
+      var listInfo = curData.listInfo;
+      var listData = curData['listData'];
       dataLength = listData.length;
-      // Title to go to list pages
-      var title = (wData.list_title).replace('/','-').replace(/\s+/g, '-');
-      // Get Team name for linking purposes
-      var team_image = listData[index].team_name;
-      var player_url = listData[index].first_name+'-'+listData[index].last_name;
-      var par_player_url = listData[index].last_name+'-'+listData[index].first_name;
-      team_image = team_image.replace(/[&\/\\#,+()$~%.'":*?<>{}]/g,'').replace('amp;', '').replace('-',' ');
-      team_image = team_image.replace(/ /g, '_');
-      var teamName = (listData[index].team_name).replace(/\s+/g, '-');
-      $('.fcw-t1').html("Top teams with the highest [Baseball Data Point] for the [YYYY] season.");
+      $('.fcw-t1').html(listInfo.name);
       $('.fcw-t2-num').html('#'+(index+1));
-      $('.fcw-image').css('background', 'url('+imageUrl(listData[index].player_img)+') no-repeat');
-      $('.fcw-logo').css('background', 'url('+teamImage(team_image)+') no-repeat');
-      $('#team_name').html("[Team Name]");
-      $('#teamProf').html("[Stadium Name]");
-      $('#location_link').html("[Location]");
-      $('.fcw-content1').html("[Data Point]");
+      $('.fcw-image').css('background', 'url('+imageUrl(listData[index].teamLogo)+') no-repeat');
+      $('#team_name').html(listData[index].teamName);
+      $('#teamProf').html(listData[index].teamVenue);
+      $('#location_link').html(listData[index].teamCity + ', ' + listData[index].teamState);
+      $('.fcw-content1').html('[Data Point]');
       $('.fcw-content2').html('[Data Point Description]');
-      $('.fcw-presentedby').html('MLB TOP TEAM - PRESENTED BY');
+      $('.fcw-presentedby').html('MLB TOP TEAMS - PRESENTED BY');
 
       if(remnant == 'true' || remnant == true){
-        $('.fcw-icon').attr('href',link);
-        $('#team_name').attr('href',link+ "/NBA/player/" + teamName + "/" + player_url + "/" + listData[index].person_id);
-        $('.exec-link').attr('href',link+"/NBA/player/" + teamName + "/" + player_url + "/" + listData[index].person_id);
-        $('#teamProf').attr('href',link + "/NBA/team/" + teamName + "/" + listData[index].team_id);
-        $('#teamProfile').attr('href',link + "/NBA/team/" + teamName + "/" + listData[index].team_id);
-        $('.fcw-href').attr('href',link + "/NBA/player/" + title + "/" + wData.list_id + "/listview/1");
+        //TODO
       } else {
-        $('.fcw-icon').attr('href',partner_link + "/" + domain);
-        $('#team_name').attr('href',partner_link+ "/" + domain + "/NBA/p/"+teamName+"/"+par_player_url+"/"+listData[index].person_id);
-        $('.exec-link').attr('href',partner_link+ "/" + domain + "/NBA/p/"+teamName+"/"+par_player_url+"/"+listData[index].person_id);
-        $('#teamProf').attr('href',partner_link + "/" + domain + "/NBA/t/" + teamName + "/" + listData[index].team_id);
-        $('#teamProfile').attr('href',partner_link + "/" + domain + "/MLB/t/" + teamName + "/" + listData[index].team_id);
-        $('.fcw-href').attr('href',partner_link + "/" + domain + "/MLB/player/" + title + "/list/" + wData.list_id+"/1");
+        //TODO
       }
   }
 
@@ -90,8 +74,4 @@ function imageUrl(path){
     return 'http://prod-sports-images.synapsys.us/nba/players/headshots/no_player_icon.png';
   }
   return 'http://prod-sports-images.synapsys.us' + path;
-}
-
-function teamImage(tpath){
-  return 'http://prod-sports-images.synapsys.us/nba/logos/NBA_'+ tpath + '_Logo.jpg';
 }
