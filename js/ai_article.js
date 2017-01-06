c137b20da24a6980bb1be0cbcbd0c72ed8b70efe
Caleb Banzhaf <caleb.banzhaf@sntmedia.com>
2017-01-06 09:13:56 -0600

    cherry picking the new changes out of dev to go to master

--- js/ai_article.js
+++ js/ai_article.js
@@ -6,44 +6,45 @@ ai_widget = (function() {
   var target;
   if (temp != null) {
     query = JSON.parse(decodeURIComponent(temp.substr(1)));
-    scope = query.league;
+    scope = query.category;
     target = query.targ;
   } else {
     scope = 'nba';
   }
-  var APIUrl = 'http://prod-sports-ai.synapsys.us/' + scope + '/widget',
+  var APIUrl = 'http://dev-sports-ai.synapsys.us/sidekick?scope=' + scope,
     AIData = {},
     gameID = -1,
     pageInd = -1,
     availPages = [],
     gameArr = [],
-    pages = ['pregame_report', 'postgame_report', 'about_the_teams', 'historical_team_stats', 'last_matchup', 'player_comparison_centers1', 'player_comparison_forwards1', 'player_comparison_guards1', 'player_comparison_forwards2', 'player_comparison_guards2', 'player_comparison_scorers', 'player_comparison_assisters', 'player_comparison_three_point_getters', 'player_comparison_rebounders', 'player_comparison_stealers', 'player_comparison_power_forwards', 'player_comparison_small_forwards', 'player_comparison_shooting_guards', 'player_comparison_point_guards', 'player_comparison_centers', 'home_team_starting_roster', 'away_team_starting_roster', 'home_team_injury_report', 'away_team_injury_report', 'upcoming'],
+    pages = ['pregame_report', 'in_game_report', 'postgame_report', 'about_the_teams', 'historical_team_statistics', 'last_matchup', 'center_player_comparison', 'guards1_player_comparison', 'guards2_player_comparison', 'power_forward_player_comparison', 'point_guard_player_comparison', 'forwards1_player_comparison', 'forwards2_player_comparison', 'scorers_player_comparison', 'assisters_player_comparison', 'three_point_player_comparison', 'rebounders_player_comparison', 'stealers_player_comparison', 'power_forward_player_comparison', 'small_forward_player_comparison', 'shooting_guard_player_comparison', 'point_guard_player_comparison', 'center_player_comparison', 'starting_roster_home', 'starting_roster_away', 'home_team_injury_report', 'injuries_away', 'upcoming_games'],
     transArr = {
-      'pregame_report': 'pregame',
-      'postgame_report': 'postgame',
-      'about_the_teams': 'about',
-      'historical_team_stats': 'history',
-      'last_matchup': 'lastmatch',
-      'player_comparison_centers1': 'centers1',
-      'player_comparison_forwards1': 'forwards1',
-      'player_comparison_guards1': 'guards1',
-      'player_comparison_forwards2': 'forwards2',
-      'player_comparison_guards2': 'guards2',
-      'player_comparison_scorers': 'scorers',
-      'player_comparison_assisters': 'assisters',
-      'player_comparison_three_point_getters': 'threepoint',
-      'player_comparison_rebounders': 'rebounders',
-      'player_comparison_stealers': 'stealers',
-      'player_comparison_centers': 'centers',
-      'player_comparison_power_forwards': 'powerforwards',
-      'player_comparison_point_guards': 'pointguards',
-      'player_comparison_small_forwards': 'smallforwards',
-      'player_comparison_shooting_guards': 'shootingguards',
-      'home_team_starting_roster': 'homeroster',
-      'away_team_starting_roster': 'awayroster',
-      'home_team_injury_report': 'homeinjury',
-      'away_team_injury_report': 'awayinjury',
-      'upcoming': 'upcoming',
+      'pregame_report': 'pregame-report',
+      'in_game_report': 'in-game-report',
+      'postgame_report': 'postgame-report',
+      'about_the_teams': 'about-the-teams',
+      'historical_team_statistics': 'historical-team-statistics',
+      'last_matchup': 'last-matchup',
+      'center_player_comparison': 'center-player-comparison',
+      'forwards1_player_comparison': 'forwards1-player-comparison',
+      'guards1_player_comparison': 'guards1-player-comparison',
+      'forwards2_player_comparison': 'forwards2-player-comparison',
+      'guards2_player_comparison': 'guards2-player-comparison',
+      'scorers_player_comparison': 'scorers-player-comparison',
+      'assisters_player_comparison': 'assisters-player-comparison',
+      'three_point_player_comparison': 'three-point-player-comparison',
+      'rebounders_player_comparison': 'rebounders-player-comparison',
+      'stealers_player_comparison': 'stealers-player-comparison',
+      'center_player_comparison': 'center-player-comparison',
+      'power_forward_player_comparison': 'power-forward-player-comparison',
+      'point_guard_player_comparison': 'point-guard-player-comparison',
+      'small_forward_player_comparison': 'small-forward-player-comparison',
+      'shooting_guard_player_comparison': 'shooting-guard-player-comparison',
+      'starting_roster_home': 'starting-roster-home',
+      'starting_roster_away': 'starting-roster-away',
+      'injuries_home': 'injuries-home',
+      'injuries_away': 'injuries-away',
+      'upcoming_games': 'upcoming-games',
     };
   function getContent(eventId) {
     // Clear old data
@@ -56,12 +57,19 @@ ai_widget = (function() {
     }
     var locApiUrl = APIUrl;
     if (typeof eventId != "undefined") {
-      locApiUrl += "/" + eventId;
+      locApiUrl += "&event=" + eventId;
     }
     $.ajax({
       url: locApiUrl,
       success: function(data) {
-        AIData = data;
+        var newArray = [data.data['meta-data']];
+        for (var key in data.data) {
+          if (data.data[key].scope) {
+            newArray.push(data.data[key]);
+            data.data[key].id = key.replace(/-/g,"_");
+          }
+        }
+        AIData = newArray;
         processData();
       },
       error: function(jqXHR, status, error) {
@@ -86,17 +94,15 @@ ai_widget = (function() {
     var pageID = availPages[pageInd];
     var content = [];
     var dataArr = [];
+    var imageArr = [];
     $.map(AIData, function(val, index) {
       if (val.id == pageID) {
-        content.title = val.title;
-        content.content = val['body'][0].content;
+        content.title = val.id.replace(/_/g," ").replace(/\b\w/g, l => l.toUpperCase());
+        content.content = val.teaser;
         content.images = val['carousel'];
         dataArr.push(content);
+        imageArr.push("http://prod-sports-images.synapsys.us" + val.image_url);
       }
-    });
-    var imageArr = [];
-    $.map(content.images, function(val, index) {
-      imageArr.push(val.url);
     });
     if (imageArr.length < 1) {
       imageArr = ['http://prod-sports-images.synapsys.us/nba/headers/nba_cover_page_1.png',
@@ -111,7 +117,7 @@ ai_widget = (function() {
     var arr = {
       title: dataArr[0].title,
       number: (pageInd + 1) + '/' + availPages.length,
-      url: AIData[0].league + '/article/' + transArr[pageID] + '/' + gameID,
+      url: scope + '/article/' + transArr[pageID] + '/' + gameID,
       content: dataArr[0].content + '<br>&nbsp; ',
       img: imageArr[imgIndex]
     };
@@ -177,7 +183,7 @@ ai_widget = (function() {
     }
     pageInd = 0;
     // Get game ID
-    gameID = AIData[0].current.id;
+    gameID = AIData[0].current.event_id;
     if (gameArr.length == 0) {
       parseGames();
     }
@@ -193,14 +199,14 @@ ai_widget = (function() {
         // Team names
         $.map(games, function(val, index) {
           var gameData = {};
-          gameData.home = val.home.abbreviation;
-          gameData.away = val.away.abbreviation;
-          gameData.fullHome = val.home.name;
-          gameData.fullAway = val.away.name;
+          gameData.home = val.home_abbreviation;
+          gameData.away = val.away_abbreviation;
+          gameData.fullHome = val.home_team_name;
+          gameData.fullAway = val.away_team_name;
           // Event ID
-          gameData.eventId = val.id;
+          gameData.eventId = val.event_id;
           // Date
-          gameData.eventDate = val.date;
+          gameData.eventDate = val.start_date_time.date;
           //due to a '-' in the eventDate the position of the date and Time will be [0] and [2]
           gameData.date = gameData.eventDate.split(' ')[0].split('/');
           //get rid of extra 0 in date month
@@ -211,15 +217,15 @@ ai_widget = (function() {
           gameData.date[2] = gameData.date[2].slice(2, 4);
           gameData.date = gameData.date.join('/');
           //convert hours to get rid of 0 in 09:XX AM/PM
-          gameData.Time = gameData.eventDate.split(' ')[2].split(':');
-          if (Number(gameData.Time[0]) < 10) {
-            gameData.Time[0] = gameData.Time[0].slice(1, 2);
-          }
-          gameData.Time = gameData.Time.join(':');
+          // gameData.Time = gameData.eventDate.split(' ')[2].split(':');
+          // if (Number(gameData.Time[0]) < 10) {
+          //   gameData.Time[0] = gameData.Time[0].slice(1, 2);
+          // }
+          gameData.Time = val.start_date_time.time;
           gameData.meridian = gameData.eventDate.split(' ')[3];
           //rejoin gameData.eventDate to one single use
-          gameData.eventDate = gameData.date + ' - ' + gameData.Time + gameData.meridian;
-          gameData.eventTime = ' - ' + gameData.Time + gameData.meridian + ' EST';
+          gameData.eventDate = gameData.date + ' - ' + gameData.Time;
+          gameData.eventTime = ' - ' + gameData.Time + ' EST';
           gameArr.push(gameData);
         });
         return gameArr;
@@ -290,17 +296,17 @@ $(function() {
   var domain, remnant;
   var temp = location.search;
   var query = {};
-  var remLink = "http://www.hoopsloyal.com/";
-  var partLink = "http://www.myhoopszone.com/";
   if (temp != null) {
     query = JSON.parse(decodeURIComponent(temp.substr(1)));
+    var remLink = "http://" + query.subd;
+    var partLink = "http://" + query.subd;
     //set the query data from database to global variable to use
     domain = query.dom;
     remnant = query.remn;
     if (remnant == 'true') {
       $("base").attr("href", remLink);
     } else {
-      $("base").attr("href", partLink + domain + "/");
+      $("base").attr("href", partLink + "/");
     }
   }
 });
