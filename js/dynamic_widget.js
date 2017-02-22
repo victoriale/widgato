ef049d7622c88c3ab8eaead91d40b0a7aef2040d
Caleb Banzhaf <caleb.banzhaf@sntmedia.com>
2017-02-22 15:08:24 -0600

    clipboard

--- js/dynamic_widget.js
+++ js/dynamic_widget.js
@@ -250,6 +250,28 @@ function getPublisher (pub) {
   }
 }
 
+function copyToClipboard(text) {
+    if (window.clipboardData && window.clipboardData.setData) {
+        // IE specific code path to prevent textarea being shown while dialog is visible.
+        return clipboardData.setData("Text", text);
+
+    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+        var textarea = document.createElement("textarea");
+        textarea.textContent = text;
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        } catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return false;
+        } finally {
+            document.body.removeChild(textarea);
+        }
+    }
+}
+
 var protocolToUse = (location.protocol == "https:") ? "https://" : "http://";
 var currentConfig;
 var currentPub;
@@ -521,6 +543,10 @@ dynamic_widget = function() {
             $('title-link').href = a;
             $('shareFacebook').href = "https://www.facebook.com/sharer/sharer.php?u=" + a;
             $('shareTwitter').href = "https://twitter.com/home?status=" + a;
+            $('shareLink').addEventListener("click", function(){
+              copyToClipboard(a)
+              alert("Link has been copied to your clipboard. \n\n" + a);
+             });
         }
         if (l.showLink == 'false') {
           $('list-link').style.display = "none";
