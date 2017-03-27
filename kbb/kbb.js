68de0319bb8bd729fb4e86a5651dbced7df1bf97
larryhpham <larrypham1991@yahoo.com>
2017-03-27 17:23:47 -0500

    update lazy load code

--- kbb/kbb.js
+++ kbb/kbb.js
@@ -1,82 +1,79 @@
 //establish environment variables for the ad-stack based on which widget is being called
-(function() {
-  var hostname = window.top.location.hostname;
+var hostname = window.top.location.hostname;
 
-  //Identifier for snt container when script has first loaded
-  var snt_id = "snt_container";
-  var sntWidgetType1 = "http://w1.synapsys.us/widgets/dynamic_caw_widget/dynamic_caw_widget_970.html?{'dom':'chicagotribune.com','loc':{'loc':{'city':[],'DMA':[],'state':[],'zipcode':[]}},'c_id':'null','remn':'false','caw_url':'http://www.chicagotribune.com/entertainment/tv/ct-donald-trump-alec-baldwin-feud-20161219-story.html','targ':'_blank','cat':'null','subd':'','rand':2}";
-  var sntWidgetType2 = "";
+//Identifier for snt container when script has first loaded
+var snt_id = "snt_container";
+var widget_id = "widget";
+var snt_widget_type1 = "//w1.synapsys.us/embeds/football_pro/dynamic_300x600/nfl.js";
 
-  // var safeGuard = false; // allows function to be only called once, if set to true then function will not run again
+var snt_widget_array = [];
+var snt_widget_count = 1; //
 
 
-  //Waits for the DOMContent to load so we know where to append the widget
-  document.addEventListener("DOMContentLoaded", function(event){
-    console.log(hostname);
-    //declare initial snt container
-    var snt_container = document.createElement("div");
-    snt_container.setAttribute("id", snt_id);
-    document.body.appendChild(snt_container);
+//Waits for the DOMContent to load so we know where to append the widget
+document.addEventListener("DOMContentLoaded", function(event){
+  console.log(hostname);
+  //declare initial snt container
+  var snt_container = document.createElement("div");
+  snt_container.setAttribute("id", snt_id);
+  document.body.appendChild(snt_container);
 
-    var snt_id_container = document.getElementById(snt_id);
+  var snt_id_container = document.getElementById(snt_id);
 
-    window.onscroll = function() {sntListener()};
-    // snt_id_container.addEventListener("onscroll", sntListener);
+  window.onscroll = function() {sntListener()};
+  // snt_id_container.addEventListener("onscroll", sntListener);
 
-    /*<---------------------------------------------------------------------------------------------------------->*/
+  /*<---------------------------------------------------------------------------------------------------------->*/
 
-    //setup create new element node function
-    var createNode = function(attribute, attributeName, widgetType){
-      var snt_widget = document.createElement("div");
-      snt_widget.setAttribute(attribute, attributeName);
-      //add widget Code Here
-      var newFrame = document.createElement("iframe");
-      //
-      newFrame.id = '1';
-      newFrame.style.width = '300px';
-      newFrame.style.height ='600px';
-      newFrame.style.border = '0px';
-      newFrame.style.margin = '0px';
-      newFrame.style.zIndex = '0';
-      newFrame.frameBorder = '0';
-      newFrame.scrolling = 'no';
-      newFrame.setAttribute('allowtransparency', 'true');
-      newFrame.style.display= 'block';
-      newFrame.style.overflow= 'hidden';
-      newFrame.src = sntWidgetType1;
-      snt_widget.appendChild(newFrame);
-      console.log(snt_widget);
+  //setup create new element node function
+  var createNode = function(attribute, attributeName, widgetType){
+    var snt_widget = document.createElement("div");
+    snt_widget.setAttribute(attribute, attributeName);
 
-      snt_id_container.appendChild(snt_widget);
-      console.log(snt_id_container);
-      return;
-    }
+    //create iframe widget and append to end of snt_id_container
+    var newFrame = createIframeWidget(widgetType);
+    snt_widget.appendChild(newFrame);
 
-    /*<---------------------------------------------------------------------------------------------------------->*/
+    snt_id_container.appendChild(snt_widget);
+    return;
+  }
 
-    //setup the onscroll function to listen to created snt identified container
-    function sntListener() {
-      var body = document.body,
-          html = document.documentElement;
-      var bodyHeight = Math.max( body.scrollHeight, body.offsetHeight,
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+  /*<---------------------------------------------------------------------------------------------------------->*/
 
-      var sntClientRect = snt_id_container.getBoundingClientRect();
-      var heightListener = (sntClientRect.height * .80);
-      // console.log('sntC from TOP',snt_id_distance.top);
-      // console.log('sntC Height',snt_id_distance.height);
-      // console.log('bodyHeight ==>', bodyHeight, 'SNT Container', sntClientRect);
-      if(sntClientRect.top <= heightListener){
+  //setup the onscroll function to listen to created snt identified container
+  function sntListener() {
+    var body = document.body,
+        html = document.documentElement;
+    var bodyHeight = Math.max( body.scrollHeight, body.offsetHeight,
+                     html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+    var sntClientRect = snt_id_container.getBoundingClientRect();
+    var heightListener = (sntClientRect.height * .80); // listen for 80% of height of snt_id_container content
+    var widgetId = widget_id+snt_widget_count;
+
+    //test of 80% of snt_id_container is visible then call
+    if(sntClientRect.top <= heightListener || snt_widget_array.length == 0){
+      // console.log(widgetId,snt_widget_array.indexOf(widgetId));
+      if(snt_widget_array.indexOf(widgetId) <= -1){
         console.log('Limit Reach Call next Widget',sntClientRect.top);
-        createNode("id", "testWidget", sntWidgetType1);
+        // createNode("id", widgetId, snt_widget_type1);
 
+        snt_widget_array.push(widgetId);
+        // snt_widget_count++;
+        return;
       }
     }
+  };
 
 
-    console.log("WIDGET CREATE");
-  });
+  console.log("WIDGET CREATE");
+});
 
-
-
-})();
+//create iframe script to be tossed into snt_id_container
+function createIframeWidget(widgetType)
+{
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = widgetType;
+  return script;
+};
