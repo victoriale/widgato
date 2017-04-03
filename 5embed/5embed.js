bbf78561d95509e6c5435aaae5d752bfcef032d4
larryhpham <larrypham1991@yahoo.com>
2017-04-03 17:17:39 -0500

    update code

--- 5embed/5embed.js
+++ 5embed/5embed.js
@@ -3,50 +3,99 @@ var temp = location.search;
 var query = {};
 var target;
 var href = window.top.location;
-
+var currentIndex = 0;
+var widgetData;
+var tries = 0;
 //Waits for the DOMContent to load
 document.addEventListener("DOMContentLoaded", function(event) {
-  if (temp != null) {
-    getAPI();
-  }
+  console.log('DOM LOADED');
+  /*
+  create function to get API
+  */
+  var apiUrl = "http://dw.synapsys.us/list_api.php?partner=latimes.com&cat=mlb&rand=27";
+  var test = runAPI(apiUrl);
+  console.log(test);
+
+  onLoad()
 });
 
-function getCategory(){
+function setCategoryColors(category){
 
 }
 
-function getAPI(apiUrl){
+/**
+* @function onLoad
+* Once the DOM has loaded, Adds a function to the "onLoad" event OR runs the function if the page is
+* already loaded
+*
+* @param function func - The function to run when the page has loaded
+*/
+function onLoad(func) {
+  if ( document.readyState == "complete" || document.readyState == "interactive" ) {
+    func();
+  } else if ( document.addEventListener ) {
+    document.addEventListener('DOMContentLoaded', func);
+  } else if ( document.attachEvent ) {
+    document.attachEvent('onreadystatechange', function(){
+      if ( document.readyState == "complete" ) {
+        func();
+      }
+    });
+  }
+} // --> onLoad
+
+/**
+* @function runAPI
+* function that makes an asynchronous request using http and setting a global variable equal to the response of the text.
+* fail safe of retrying 10 times before sending error message
+*
+* @param function apiUrl -
+*/
+function runAPI(apiUrl){
+  //variable that stores the response of an http request
   if (window.XMLHttpRequest) {
       var xhttp = new XMLHttpRequest();
   } else {
       var xhttp = new ActiveXObject('Microsoft.XMLHTTP')
   }
   xhttp.onreadystatechange = function() {
-    console.log(this);
       if (this.readyState == XMLHttpRequest.DONE) {
           if (this.status == 200) {
-              // r = JSON.parse(i.responseText);
-              // c(u)
+              // On success parse out the response
+              widgetData = JSON.parse(this.responseText);
+              // onLoad(func);//send in the name of the function that needs to be ran once data has been confirmed
+              console.log(widgetData);
+              return widgetData;
           } else {
-              // var e = i.statusText;
-              // if (i.status == 500) {
-              //     try {
-              //         e = JSON.parse(i.responseText).message
-              //     } catch (t) {
-              //         console.log('No JSON message')
-              //     }
-              // }
-              // e = 'HTTP Error (' + i.status + '): ' + e;
-              // if (n++ > 10) {
-              //     throw e
-              // }
-              // setTimeout(m, 500)
+              // Error handling
+              // Get the message
+              var msg = this.statusText;
+              if (this.status == 500) {
+                  try {
+                      msg = JSON.parse(this.responseText).message
+                  } catch (e) {
+                      console.log('No JSON message')
+                  }
+              }
+              msg = 'HTTP Error (' + this.status + '): ' + msg;
+              if (tries++ > 10) {
+                  throw msg
+              }
+              setTimeout(runAPI(apiUrl), 500)
           }
       }
   };
 
   xhttp.open("GET", apiUrl, true);
   xhttp.send();
-
-  console.log(xhttp);
 };
+
+
+
+function createWidget() {
+  console.log('creating Widget');
+  console.log(widgetData);
+
+  // // Display the first item
+  // di();
+} // --> create_widget
