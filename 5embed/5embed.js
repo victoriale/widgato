0224c95da4ca9b926cb53a109f5733249e286fa9
larryhpham <larrypham1991@yahoo.com>
2017-04-04 17:22:17 -0500

    added dynamic color changing

--- 5embed/5embed.js
+++ 5embed/5embed.js
@@ -4,22 +4,116 @@ var query = {};
 var target;
 var href = window.top.location;
 var currentIndex = 0;
+var maxIndex = 1;
 var widgetData;
 var tries = 0;
+var categoryColors = {
+  // Brand Color Palette
+  'football'    : '#2d3e50',
+  'basketball'  : '#f26f26',
+  'baseball'    : '#bc2027',
+  'finance'     : '#3098ff',
+  'realestate'  : '#44b224',
+  'lifestyle'   : '#65398e',
+  'disaster'    : '#902d8e',
+  'politics'    : '#ff0101',
+  'crime'       : '#f6af05',
+  'weather'     : '#ffdf30',
+  'default'     : '#000000',
+};
+
 //Waits for the DOMContent to load
 document.addEventListener("DOMContentLoaded", function(event) {
-  console.log('DOM LOADED');
   /*
   create function to get API
   */
-  var apiUrl = "http://dw.synapsys.us/list_api.php?partner=latimes.com&cat=mlb&rand=27";
-  var test = runAPI(apiUrl);
-  console.log(test);
+  var apiUrl = "http://dw.synapsys.us/list_api.php?partner=chicagotribune.com&cat=mlb&rand=9949894";
 
-  onLoad()
+  setCategoryColors('lifestyle');
+
+  var test = runAPI(apiUrl);
 });
 
 function setCategoryColors(category){
+  let color;
+  switch(category){
+    case 'football':
+    case 'nfl':
+    case 'ncaaf':
+      category = 'football';
+    break;
+    case 'basketball':
+    case 'nba':
+    case 'ncaam':
+      category = 'basketball';
+    break;
+    case 'baseball':
+    case 'mlb':
+      category = 'baseball';
+    break;
+    default:
+    break;
+  }
+  color =  categoryColors[category];
+
+  function classLoop(cssName, style, styleColor){
+    let classArray = document.getElementsByClassName(cssName);
+    let styleSheets = getCssSelector("5embed");
+    var attribute = findCss(cssName, styleSheets);
+    styleSheets.deleteRule(attribute.index);
+
+    //try catch statements are for ie compatibility
+    switch(style){
+      case 'color':
+        try{
+          styleSheets.insertRule( '.'+cssName+' { color: '+styleColor+'; }', 0 );
+        }catch(e){
+          styleSheets.addRule('.'+cssName, 'color: '+styleColor, 0);
+        }
+      break;
+      case 'border-color':
+      try{
+        styleSheets.insertRule( '.'+cssName+' { border-color: '+styleColor+'; }', 0 );
+      }catch(e){
+        styleSheets.addRule('.'+cssName, 'border-color: '+styleColor, 0);
+      }
+      break;
+      case 'background-color':
+        try{
+          styleSheets.insertRule( '.'+cssName+' { background-color: '+styleColor+'; }', 0 );
+        }catch(e){
+          styleSheets.addRule('.'+cssName, 'background-color: '+styleColor, 0);
+        }
+      break;
+      default:
+      break;
+    }
+
+  }
+
+  function getCssSelector(title){
+    let selector = document.styleSheets;
+    for(var index = 0; index < selector.length; index++){
+      if(selector[index].title == title){
+        return selector[index];
+      }
+    }
+  }
+
+  function findCss(cssName, styleSheets){
+    if(styleSheets.cssRules != null){
+      for(var index = 0; index < styleSheets.cssRules.length; index++){
+        if(styleSheets.cssRules[index].selectorText == "."+cssName){
+          styleSheets.cssRules[index].index = index;
+          return styleSheets.cssRules[index];
+        }
+      }
+    }
+  }
+
+  classLoop('inheritor', 'color', color);
+  classLoop('inheritor_border', 'border-color', color);
+  classLoop('inheritor_bg:hover::before', 'background-color', color);
 
 }
 
@@ -51,7 +145,7 @@ function onLoad(func) {
 *
 * @param function apiUrl -
 */
-function runAPI(apiUrl){
+function runAPI(apiUrl, func){
   //variable that stores the response of an http request
   if (window.XMLHttpRequest) {
       var xhttp = new XMLHttpRequest();
@@ -63,9 +157,7 @@ function runAPI(apiUrl){
           if (this.status == 200) {
               // On success parse out the response
               widgetData = JSON.parse(this.responseText);
-              // onLoad(func);//send in the name of the function that needs to be ran once data has been confirmed
-              console.log(widgetData);
-              return widgetData;
+              onLoad(displayWidget);//send in the name of the function that needs to be ran once data has been confirmed
           } else {
               // Error handling
               // Get the message
@@ -81,7 +173,7 @@ function runAPI(apiUrl){
               if (tries++ > 10) {
                   throw msg
               }
-              setTimeout(runAPI(apiUrl), 500)
+              setTimeout(runAPI(apiUrl, func), 500)
           }
       }
   };
@@ -92,10 +184,39 @@ function runAPI(apiUrl){
 
 
 
-function createWidget() {
-  console.log('creating Widget');
-  console.log(widgetData);
+function displayWidget() {
+  try{
+    let dataArray = widgetData.l_data;
+    //set maximum index of returned dataLayer
+    maxIndex = dataArray.length;
 
-  // // Display the first item
-  // di();
+    let curData = dataArray[currentIndex];
+    //list title
+    $("profile-title").innerHTML = widgetData.l_title;
+
+    //current index of list
+    $("mainimg").setAttribute('src',curData.li_img+"?width=300");
+    $("mainimg").setAttribute('onerror',"//images.synapsys.us/01/fallback/stock/2017/03/");
+
+    $("profile-rank").innerHTML = curData.li_rank;
+    $("profile-name").innerHTML = curData.li_title;
+    $("profile-datapoint1").innerHTML = curData.li_tag;
+    $("profile-datavalue1").innerHTML = curData.li_value;
+    $("profile-datavalue2").innerHTML = curData.li_sub_txt;
+  }catch(e){
+    console.log('Error in displaying widget Data');
+    // console.log(e);
+  }
 } // --> create_widget
+
+function updateIndex(difference){
+  currentIndex += difference;
+  if ( currentIndex < 0 ){
+    currentIndex = 0;
+  }else if ( currentIndex >= maxIndex ){
+    currentIndex = maxIndex;
+  }else{
+  }
+  //call display widget
+  displayWidget();
+}
