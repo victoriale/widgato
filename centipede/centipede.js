821e33bf8078d2db8b070c8c2f0ce4c712d20013
Caleb Banzhaf <caleb.banzhaf@sntmedia.com>
2017-04-28 16:58:54 -0500

    another revision to the centipede data point logic

--- centipede/centipede.js
+++ centipede/centipede.js
@@ -634,10 +634,6 @@ loadData();
     else if (input.group == "entertainment" || data.category == "entertainment") { //if celeb data, transform it
       var items = [];
         for (i = 0; i < data.l_data.length; i++) {
-          if (data.l_data[i].data_point_2 == null || data.l_data[i].data_value_2 == null) {
-            data.l_data[i].data_point_2 = data.l_data[i].data_point_1;
-            data.l_data[i].data_value_2 = data.l_data[i].data_value_1;
-          }
           items.push(
             {
               li_img: data.l_data[i].li_img,
