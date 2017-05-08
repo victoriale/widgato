5589b17bb88fce272fd4ace95d681186768c3a7a
larryhpham <larrypham1991@yahoo.com>
2017-05-08 10:14:53 -0500

    reverse counter for dynamic widget

--- dynamic_widget_unlinked/dynamic_widget_unlinked.js
+++ dynamic_widget_unlinked/dynamic_widget_unlinked.js
@@ -132,7 +132,6 @@ function setupEnvironment(widgetQuery) {
  * @param function listNum - list number incremented that will be added to the listRand with listNum
  */
 function updateList(listNum) {
-    currentIndex = 0;
     if (query.group == null && (query.category == 'nfl' || query.category == 'ncaaf' || query.category == 'football')) {
         getFootballList(query.category);
     } else {
@@ -240,6 +239,15 @@ function runAPI(apiUrl) { //Make it to where it is easy to be reused by anyone
             if (this.status == 200) {
                 // On success parse out the response
                 widgetData = JSON.parse(this.responseText);
+                var dataArray = widgetData.l_data != null ? widgetData.l_data : widgetData.data.listData;
+                //set maximum index of returned dataLayer
+                if(dataArray.length >= 25){
+                  currentIndex = 24;
+                  maxIndex = 25;
+                }else{
+                  currentIndex = dataArray.length;
+                  maxIndex = dataArray.length;
+                }
                 onLoad(displayWidget); //send in the name of the function that needs to be ran once data has been confirmed
             } else {
                 // Error handling
@@ -286,13 +294,10 @@ function displayWidget() {
         // $('profile-updated').innerHTML = formatedDate.weekday + ", " + formatedDate.month + " " + formatedDate.day + ", " + formatedDate.year;
 
         //Run dynamic color of widget
-
         /***************************FOOTBALL DATA APPLIANCE*******************************/
         if (query.group == null && (query.category == "football" || query.category == "nfl" || query.category == "ncaaf")) {
             var dataArray = widgetData.data.listData;
             setCategoryColors(subCategory);
-            //set maximum index of returned dataLayer
-            maxIndex = dataArray.length;
             var curData = dataArray[currentIndex];
 
             //list title
@@ -348,8 +353,6 @@ function displayWidget() {
             }
 
             setCategoryColors(subCategory);
-            //set maximum index of returned dataLayer
-            maxIndex = dataArray.length;
             //current index of list
             var curData = dataArray[currentIndex];
 
@@ -473,137 +476,6 @@ function setCategoryColors(category) {
     var atomicClass = document.getElementsByClassName("widget_ad")[0];
 
     atomicClass.id = wideWidget ? category+'-wide' : category;
-
-    /* SNT DEFINED CLASSES TO BE FOUND AND USED FOR
-     * function classInheritorReplace(identifier, category)
-     */
-    // classInheritorReplace("color_inheritor", category);
-    // classInheritorReplace("background_inheritor", category);
-    // classInheritorReplace("button_inheritor", category);
-
-    /********************** SETUP CATEGORY COLORS **********************
-     * @function classInheritorReplace
-     * dynamically set the colors of the css Rules each identifier in the html for dynamic colors
-     *
-     * @param function
-     *      identifier - unique identifier in the html used to run a function that replaces the color scheme based on category
-     *      category - sets the base category for colors that are stored in the global ./css/inheritor/inheritor.css
-     */
-    // function classInheritorReplace(identifier, category) {
-    //     var htmlClass = document.getElementById(identifier);
-    //     var re = new RegExp('inheritor', "g");
-    //     var categoryClass = category == 'default' ? '' : category + '-'; // ex default returns nothing , football-, baseball-
-    //     var classes = htmlClass.className.split(" ").filter(function(c) {
-    //         return c.match(re) != null ? c.match(re) : null;
-    //     });
-    //     switch (identifier) {
-    //         case "color_inheritor":
-    //             htmlClass.classList.remove(classes[0]);
-    //             htmlClass.classList.add(categoryClass + "inheritor");
-    //             break;
-    //         case "background_inheritor":
-    //             htmlClass.classList.remove(classes[0]);
-    //             htmlClass.classList.add(categoryClass + "inheritor_img_bg");
-    //             break;
-    //         case "button_inheritor":
-    //             for (var i = 0; i < classes.length; i++) {
-    //                 htmlClass.classList.remove(classes[i]);
-    //             }
-    //             htmlClass.classList.add(categoryClass + "inheritor_border");
-    //             htmlClass.classList.add(categoryClass + "inheritor_bg");
-    //             break;
-    //     }
-    // }
-
-
-    /**************************************************************DEPRECATED*****************************************************/
-    // color = categoryColors[category];
-    // /************************* CSS CLASS LOOPING *********************
-    //  * @function classLoop
-    //  * Loops throught stylesheet and finds cssName and change the cssRules
-    //  *
-    //  * @param function
-    //  *     cssName - name of css class to change its cssRule
-    //  *     style - the style with specific cases currently available styles => color, border-color, background-color
-    //  *     styleColor - uses color based on the categoryColors and category
-    //  */
-    // function classLoop(cssName, style, styleColor) {
-    //     var styleSheets = getCssSelector("5embed");
-    //
-    //     //delete inheritor rule and RE-APPLY css with new rule (easier when only one cssrule is needed to change)
-    //     if (styleSheets) {
-    //         var attribute = findCss(cssName, styleSheets);
-    //         styleSheets.deleteRule(attribute.index);
-    //         //try catch statements are for ie compatibility
-    //         switch (style) {
-    //             case 'color':
-    //                 try {
-    //                     styleSheets.insertRule('.' + cssName + ' { color: ' + styleColor + ' !important; }', 0);
-    //                 } catch (e) {
-    //                     styleSheets.addRule('.' + cssName, 'color: ' + styleColor + ' !important', 0);
-    //                 }
-    //                 break;
-    //             case 'border-color':
-    //                 try {
-    //                     styleSheets.insertRule('.' + cssName + ' { border-color: ' + styleColor + ' !important; }', 0);
-    //                 } catch (e) {
-    //                     styleSheets.addRule('.' + cssName, 'border-color: ' + styleColor + ' !important', 0);
-    //                 }
-    //                 break;
-    //             case 'background-color':
-    //                 try {
-    //                     styleSheets.insertRule('.' + cssName + ' { background-color: ' + styleColor + ' !important; }', 0);
-    //                 } catch (e) {
-    //                     styleSheets.addRule('.' + cssName, 'background-color: ' + styleColor + ' !important', 0);
-    //                 }
-    //                 break;
-    //             default:
-    //                 break;
-    //         }
-    //     }
-    //
-    // }
-    //
-    // /*********************** GET CSS SELECTOR **********************
-    //  * @function getCssSelector
-    //  * find the css File with the title given to the function
-    //  *
-    //  * @param function title - send in the title to choose which selector
-    //  */
-    // function getCssSelector(title) {
-    //     var selector = document.styleSheets;
-    //     for (var index = 0; index < selector.length; index++) {
-    //         if (selector[index].title == title) {
-    //             return selector[index];
-    //         }
-    //     }
-    // }
-    //
-    // /********************* FIND CSS NAME ******************
-    //  * @function findCss
-    //  * find the the specific css element by the given selector Text (ex: .inheritor , body, html, #profile-name)
-    //  *
-    //  * @param function
-    //  *     cssName - cssName of the rule we want to find
-    //  *     styleSheets - the chosen style seet that we need to change the cssName for
-    //  */
-    // function findCss(cssName, styleSheets) {
-    //     if (styleSheets.cssRules != null) {
-    //         for (var index = 0; index < styleSheets.cssRules.length; index++) {
-    //             if (styleSheets.cssRules[index].selectorText == "." + cssName) {
-    //                 styleSheets.cssRules[index].index = index;
-    //                 return styleSheets.cssRules[index];
-    //             }
-    //         }
-    //     }
-    // }
-    //
-    // //Class Loop will change the color by looping through cssSelector File and changing the given class
-    // classLoop('inheritor', 'color', color);
-    // classLoop('inheritor_border', 'border-color', color);
-    // classLoop('inheritor_img_bg', 'background-color', color);
-    // classLoop('inheritor_bg:hover::before', 'background-color', color);
-    /**************************************************************DEPRECATED*****************************************************/
 }
 
 /************************ Update Index *************************
@@ -614,7 +486,7 @@ function setCategoryColors(category) {
  * @param function difference - difference that will be added to current index
  */
 function updateIndex(difference) {
-    currentIndex += difference;
+    currentIndex -= difference;
     if (currentIndex < 0) {
         currentIndex = maxIndex - 1;
     } else if (currentIndex >= maxIndex) {
